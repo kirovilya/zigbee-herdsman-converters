e9437ee9efd9ef16bb017810003d723ef135059a
Koen Kanters <koenkanters94@gmail.com>
2026-09-06 05:01:24 +0000

    fix: Cover: fix OPEN/CLOSE commands with `invert_cover: true` https://github.com/Koenkk/zigbee2mqtt/issues/33004

--- src/lib/exposes.ts
+++ src/lib/exposes.ts
@@ -846,7 +846,7 @@ export const options = {
             ),
     invert_cover: () =>
         new Binary("invert_cover", access.SET, true, false).withDescription(
-            "Inverts the cover position and state, false: open=100,close=0, true: open=0,close=100 (default false).",
+            "Inverts the reported cover position and the state derived from it, false: open=100,close=0, true: open=0,close=100 (default false).",
         ),
     illuminance_raw: () => new Binary("illuminance_raw", access.SET, true, false).withDescription("Expose the raw illuminance value."),
     color_sync: () =>
